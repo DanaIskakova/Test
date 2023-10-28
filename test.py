c2ef9da009d5d913ff70b6d0fab07e4ac2712859
DanaIskakova <danaisk030283@mail.ru>
2023-10-29 01:25:44 +0600

    1

--- test.py
+++ test.py
@@ -1,5 +1,32 @@
-from task import add_two
-def test_add_two():
-    assert add_two(1,1)==2
+from task import is_prime
+from task import primes
+from task import checksum
+from task import pipeline
+
+def test_is_prime():
+    assert is_prime(0) == False
+    assert is_prime(1) == False
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(7) == True
+    assert is_prime(12) == False
+    assert is_prime(83) == True   
     
-test_add_two()
+def test_primes():
+    result = primes(10)
+    assert len(result) == 10
+    
+    result = primes(1)
+    assert len(result) == 1
+    
+def test_checksum():
+    result = checksum([1, 2, 6, 24])
+    assert result == 6012369
+
+    result = checksum([])
+    assert result == 0
+    
+def test_pipeline():
+    result = pipeline()
+    assert result == 7785816       
